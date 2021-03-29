a2172ce052b83ee1af25e343c0c4f79917559306
Gavin Mao <maogm12@gmail.com>
2021-03-28 22:41:54 -0700

    Add `isMatch` and `search`

--- Sources/SwiftOnig/Regex.swift
+++ Sources/SwiftOnig/Regex.swift
@@ -72,10 +72,20 @@ public class Regex {
         }
     }
 
-    /// Match String
-    public func match<T: StringProtocol>(_ str: T) throws -> Bool {
+    /**
+     Match string and returns true if and only if the regex matches the whole string given.
+     - Parameters:
+        - str: Target string to match against
+     - Returns:
+        `true` if and only if the regex matches the whole string given, otherwise `false`.
+     */
+    public func isMatch<T: StringProtocol>(_ str: T) throws -> Bool {
         let matchParam = try MatchParam()
-        return try self.match(str, at: 0, options: .none, region: nil, matchParam: matchParam)
+        if let matchLen = try self.match(str, at: 0, options: .none, region: nil, matchParam: matchParam) {
+            return matchLen == str.utf8.count
+        } else {
+            return false
+        }
     }
     
     /**
@@ -87,10 +97,10 @@ public class Regex {
         - region: Address for return group match range info
         - matchParam: Match parameter values (match_stack_limit, retry_limit_in_match, retry_limit_in_search)
      - Returns:
-       `true` if the match succeeds, otherwise `false`
+        The byte-position of the start of the match if the regex matches, `nil` if it doesn't match.
      - Throws: `OnigError`
      */
-    public func match<T: StringProtocol>(_ str: T, at: Int, options: SearchOptions, region: Region?, matchParam: MatchParam) throws -> Bool {
+    public func match<T: StringProtocol>(_ str: T, at: Int, options: SearchOptions, region: Region?, matchParam: MatchParam) throws -> Int? {
         let byteCount = str.utf8.count
         if at > byteCount {
             throw OnigError.invalidArgument
@@ -113,9 +123,51 @@ public class Regex {
         }
         
         if result >= 0 {
-            return true
+            return Int(result)
         } else if result == ONIG_MISMATCH {
-            return false
+            return nil
+        }
+
+        throw OnigError(result)
+    }
+    
+    /**
+     Search string and return search result and matching region. Do not pass invalid byte string in the regex character encoding.
+     - Parameters:
+        - str: Target string to search against
+        - at: The byte index in the passed buffer to start searching
+        - option: The regex search options.
+        - region: Address for return group match range info
+        - matchParam: Match parameter values (match_stack_limit, retry_limit_in_match, retry_limit_in_search)
+     - Returns: match position offset, or `nil` if nothing is found.
+     - Throws: `OnigError`
+     */
+    public func search<T: StringProtocol>(_ str: T, at: Int, options: SearchOptions, region: Region?, matchParam: MatchParam) throws -> Int? {
+        let byteCount = str.utf8.count
+        if at > byteCount {
+            throw OnigError.invalidArgument
+        }
+
+        let result = str.withCString { (cstr: UnsafePointer<Int8>) -> Int32 in
+            cstr.withMemoryRebound(to: OnigUChar.self, capacity: byteCount) { start -> Int32 in
+                let onigRegion: UnsafeMutablePointer<OnigRegion>! = nil
+                if region != nil {
+                    onigRegion.pointee = region!.rawValue
+                }
+                return onig_match_with_param(self.rawValue,
+                                             start,
+                                             start.advanced(by: byteCount),
+                                             start.advanced(by: at),
+                                             onigRegion,
+                                             options.rawValue,
+                                             matchParam.rawValue)
+            }
+        }
+        
+        if result >= 0 {
+            return Int(result)
+        } else if result == ONIG_MISMATCH {
+            return nil
         }
 
         throw OnigError(result)
