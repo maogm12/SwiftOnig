552c9fed9a2eb8adebbad71a7f00aa8abe9237df
Guangming Mao <maogm12@gmail.com>
2026-03-26 00:25:10 -0700

    refactor: reduce repetitive C globals bridge code
    - replace dozens of hand-written constant getter implementations in CGlobals.c with grouped macros while preserving the exported shim API
    - keep the Swift-facing bridge surface stable so encoding, syntax, and option access remain behavior-compatible
    - mark the tracked C globals cleanup task complete in plan.md

--- Sources/OnigurumaC/CGlobals.c
+++ Sources/OnigurumaC/CGlobals.c
@@ -1,94 +1,107 @@
 #include "CGlobals.h"
 
-OnigEncoding get_onig_ascii() { return ONIG_ENCODING_ASCII; }
-OnigEncoding get_onig_iso8859_1() { return ONIG_ENCODING_ISO_8859_1; }
-OnigEncoding get_onig_iso8859_2() { return ONIG_ENCODING_ISO_8859_2; }
-OnigEncoding get_onig_iso8859_3() { return ONIG_ENCODING_ISO_8859_3; }
-OnigEncoding get_onig_iso8859_4() { return ONIG_ENCODING_ISO_8859_4; }
-OnigEncoding get_onig_iso8859_5() { return ONIG_ENCODING_ISO_8859_5; }
-OnigEncoding get_onig_iso8859_6() { return ONIG_ENCODING_ISO_8859_6; }
-OnigEncoding get_onig_iso8859_7() { return ONIG_ENCODING_ISO_8859_7; }
-OnigEncoding get_onig_iso8859_8() { return ONIG_ENCODING_ISO_8859_8; }
-OnigEncoding get_onig_iso8859_9() { return ONIG_ENCODING_ISO_8859_9; }
-OnigEncoding get_onig_iso8859_10() { return ONIG_ENCODING_ISO_8859_10; }
-OnigEncoding get_onig_iso8859_11() { return ONIG_ENCODING_ISO_8859_11; }
-OnigEncoding get_onig_iso8859_13() { return ONIG_ENCODING_ISO_8859_13; }
-OnigEncoding get_onig_iso8859_14() { return ONIG_ENCODING_ISO_8859_14; }
-OnigEncoding get_onig_iso8859_15() { return ONIG_ENCODING_ISO_8859_15; }
-OnigEncoding get_onig_iso8859_16() { return ONIG_ENCODING_ISO_8859_16; }
-OnigEncoding get_onig_utf8() { return ONIG_ENCODING_UTF8; }
-OnigEncoding get_onig_utf16be() { return ONIG_ENCODING_UTF16_BE; }
-OnigEncoding get_onig_utf16le() { return ONIG_ENCODING_UTF16_LE; }
-OnigEncoding get_onig_utf32be() { return ONIG_ENCODING_UTF32_BE; }
-OnigEncoding get_onig_utf32le() { return ONIG_ENCODING_UTF32_LE; }
-OnigEncoding get_onig_eucjp() { return ONIG_ENCODING_EUC_JP; }
-OnigEncoding get_onig_euctw() { return ONIG_ENCODING_EUC_TW; }
-OnigEncoding get_onig_euckr() { return ONIG_ENCODING_EUC_KR; }
-OnigEncoding get_onig_euccn() { return ONIG_ENCODING_EUC_CN; }
-OnigEncoding get_onig_sjis() { return ONIG_ENCODING_SJIS; }
-OnigEncoding get_onig_koi8r() { return ONIG_ENCODING_KOI8_R; }
-OnigEncoding get_onig_cp1251() { return ONIG_ENCODING_CP1251; }
-OnigEncoding get_onig_big5() { return ONIG_ENCODING_BIG5; }
-OnigEncoding get_onig_gb18030() { return ONIG_ENCODING_GB18030; }
+#define DEFINE_ENCODING_GETTER(name, constant) \
+    OnigEncoding get_onig_##name() { return constant; }
 
-OnigSyntaxType* get_onig_asis() { return ONIG_SYNTAX_ASIS; }
-OnigSyntaxType* get_onig_posix_basic() { return ONIG_SYNTAX_POSIX_BASIC; }
-OnigSyntaxType* get_onig_posix_extended() { return ONIG_SYNTAX_POSIX_EXTENDED; }
-OnigSyntaxType* get_onig_emacs() { return ONIG_SYNTAX_EMACS; }
-OnigSyntaxType* get_onig_grep() { return ONIG_SYNTAX_GREP; }
-OnigSyntaxType* get_onig_gnu_regex() { return ONIG_SYNTAX_GNU_REGEX; }
-OnigSyntaxType* get_onig_java() { return ONIG_SYNTAX_JAVA; }
-OnigSyntaxType* get_onig_perl() { return ONIG_SYNTAX_PERL; }
-OnigSyntaxType* get_onig_perl_ng() { return ONIG_SYNTAX_PERL_NG; }
-OnigSyntaxType* get_onig_ruby() { return ONIG_SYNTAX_RUBY; }
-OnigSyntaxType* get_onig_oniguruma() { return ONIG_SYNTAX_ONIGURUMA; }
-OnigSyntaxType* get_onig_default_syntax() { return ONIG_SYNTAX_DEFAULT; }
+#define DEFINE_SYNTAX_GETTER(name, constant) \
+    OnigSyntaxType* get_onig_##name() { return constant; }
 
-unsigned int get_onig_option_extend() { return ONIG_OPTION_EXTEND; }
-unsigned int get_onig_option_multiline() { return ONIG_OPTION_MULTILINE; }
-unsigned int get_onig_option_singleline() { return ONIG_OPTION_SINGLELINE; }
-unsigned int get_onig_option_find_longest() { return ONIG_OPTION_FIND_LONGEST; }
-unsigned int get_onig_option_find_not_empty() { return ONIG_OPTION_FIND_NOT_EMPTY; }
-unsigned int get_onig_option_negate_singleline() { return ONIG_OPTION_NEGATE_SINGLELINE; }
-unsigned int get_onig_option_dont_capture_group() { return ONIG_OPTION_DONT_CAPTURE_GROUP; }
-unsigned int get_onig_option_capture_group() { return ONIG_OPTION_CAPTURE_GROUP; }
-unsigned int get_onig_option_notbol() { return ONIG_OPTION_NOTBOL; }
-unsigned int get_onig_option_noteol() { return ONIG_OPTION_NOTEOL; }
-unsigned int get_onig_option_not_begin_string() { return ONIG_OPTION_NOT_BEGIN_STRING; }
-unsigned int get_onig_option_not_end_string() { return ONIG_OPTION_NOT_END_STRING; }
-unsigned int get_onig_option_not_begin_position() { return ONIG_OPTION_NOT_BEGIN_POSITION; }
+#define DEFINE_UINT_GETTER(name, constant) \
+    unsigned int get_onig_##name() { return constant; }
 
-unsigned int get_onig_syn_op_variable_meta_characters() { return ONIG_SYN_OP_VARIABLE_META_CHARACTERS; }
-unsigned int get_onig_syn_op_dot_anychar() { return ONIG_SYN_OP_DOT_ANYCHAR; }
-unsigned int get_onig_syn_op_asterisk_zero_inf() { return ONIG_SYN_OP_ASTERISK_ZERO_INF; }
-unsigned int get_onig_syn_op_esc_asterisk_zero_inf() { return ONIG_SYN_OP_ESC_ASTERISK_ZERO_INF; }
-unsigned int get_onig_syn_op_plus_one_inf() { return ONIG_SYN_OP_PLUS_ONE_INF; }
-unsigned int get_onig_syn_op_esc_plus_one_inf() { return ONIG_SYN_OP_ESC_PLUS_ONE_INF; }
-unsigned int get_onig_syn_op_qmark_zero_one() { return ONIG_SYN_OP_QMARK_ZERO_ONE; }
-unsigned int get_onig_syn_op_esc_qmark_zero_one() { return ONIG_SYN_OP_ESC_QMARK_ZERO_ONE; }
-unsigned int get_onig_syn_op_bracket_cc() { return ONIG_SYN_OP_BRACKET_CC; }
-unsigned int get_onig_syn_op_esc_brace_interval() { return ONIG_SYN_OP_ESC_BRACE_INTERVAL; }
-unsigned int get_onig_syn_op_vbar_alt() { return ONIG_SYN_OP_VBAR_ALT; }
-unsigned int get_onig_syn_op_esc_vbar_alt() { return ONIG_SYN_OP_ESC_VBAR_ALT; }
-unsigned int get_onig_syn_op_lparen_subexp() { return ONIG_SYN_OP_LPAREN_SUBEXP; }
-unsigned int get_onig_syn_op_esc_lparen_subexp() { return ONIG_SYN_OP_ESC_LPAREN_SUBEXP; }
-unsigned int get_onig_syn_op_esc_o_brace_octal() { return ONIG_SYN_OP_ESC_O_BRACE_OCTAL; }
+DEFINE_ENCODING_GETTER(ascii, ONIG_ENCODING_ASCII)
+DEFINE_ENCODING_GETTER(iso8859_1, ONIG_ENCODING_ISO_8859_1)
+DEFINE_ENCODING_GETTER(iso8859_2, ONIG_ENCODING_ISO_8859_2)
+DEFINE_ENCODING_GETTER(iso8859_3, ONIG_ENCODING_ISO_8859_3)
+DEFINE_ENCODING_GETTER(iso8859_4, ONIG_ENCODING_ISO_8859_4)
+DEFINE_ENCODING_GETTER(iso8859_5, ONIG_ENCODING_ISO_8859_5)
+DEFINE_ENCODING_GETTER(iso8859_6, ONIG_ENCODING_ISO_8859_6)
+DEFINE_ENCODING_GETTER(iso8859_7, ONIG_ENCODING_ISO_8859_7)
+DEFINE_ENCODING_GETTER(iso8859_8, ONIG_ENCODING_ISO_8859_8)
+DEFINE_ENCODING_GETTER(iso8859_9, ONIG_ENCODING_ISO_8859_9)
+DEFINE_ENCODING_GETTER(iso8859_10, ONIG_ENCODING_ISO_8859_10)
+DEFINE_ENCODING_GETTER(iso8859_11, ONIG_ENCODING_ISO_8859_11)
+DEFINE_ENCODING_GETTER(iso8859_13, ONIG_ENCODING_ISO_8859_13)
+DEFINE_ENCODING_GETTER(iso8859_14, ONIG_ENCODING_ISO_8859_14)
+DEFINE_ENCODING_GETTER(iso8859_15, ONIG_ENCODING_ISO_8859_15)
+DEFINE_ENCODING_GETTER(iso8859_16, ONIG_ENCODING_ISO_8859_16)
+DEFINE_ENCODING_GETTER(utf8, ONIG_ENCODING_UTF8)
+DEFINE_ENCODING_GETTER(utf16be, ONIG_ENCODING_UTF16_BE)
+DEFINE_ENCODING_GETTER(utf16le, ONIG_ENCODING_UTF16_LE)
+DEFINE_ENCODING_GETTER(utf32be, ONIG_ENCODING_UTF32_BE)
+DEFINE_ENCODING_GETTER(utf32le, ONIG_ENCODING_UTF32_LE)
+DEFINE_ENCODING_GETTER(eucjp, ONIG_ENCODING_EUC_JP)
+DEFINE_ENCODING_GETTER(euctw, ONIG_ENCODING_EUC_TW)
+DEFINE_ENCODING_GETTER(euckr, ONIG_ENCODING_EUC_KR)
+DEFINE_ENCODING_GETTER(euccn, ONIG_ENCODING_EUC_CN)
+DEFINE_ENCODING_GETTER(sjis, ONIG_ENCODING_SJIS)
+DEFINE_ENCODING_GETTER(koi8r, ONIG_ENCODING_KOI8_R)
+DEFINE_ENCODING_GETTER(cp1251, ONIG_ENCODING_CP1251)
+DEFINE_ENCODING_GETTER(big5, ONIG_ENCODING_BIG5)
+DEFINE_ENCODING_GETTER(gb18030, ONIG_ENCODING_GB18030)
 
-unsigned int get_onig_syn_op2_qmark_lt_named_group() { return ONIG_SYN_OP2_QMARK_LT_NAMED_GROUP; }
-unsigned int get_onig_syn_op2_esc_p_brace_circumflex_not() { return ONIG_SYN_OP2_ESC_P_BRACE_CIRCUMFLEX_NOT; }
-unsigned int get_onig_syn_op2_esc_x_y_text_segment() { return ONIG_SYN_OP2_ESC_X_Y_TEXT_SEGMENT; }
-unsigned int get_onig_syn_op2_esc_v_vtab() { return ONIG_SYN_OP2_ESC_V_VTAB; }
-unsigned int get_onig_syn_op2_esc_u_hex4() { return ONIG_SYN_OP2_ESC_U_HEX4; }
-unsigned int get_onig_syn_op2_esc_h_xdigit() { return ONIG_SYN_OP2_ESC_H_XDIGIT; }
-unsigned int get_onig_syn_op2_esc_k_named_backref() { return ONIG_SYN_OP2_ESC_K_NAMED_BACKREF; }
-unsigned int get_onig_syn_op2_esc_g_subexp_call() { return ONIG_SYN_OP2_ESC_G_SUBEXP_CALL; }
-unsigned int get_onig_syn_op2_qmark_lparen_if_else() { return ONIG_SYN_OP2_QMARK_LPAREN_IF_ELSE; }
-unsigned int get_onig_syn_op2_esc_capital_r_general_newline() { return ONIG_SYN_OP2_ESC_CAPITAL_R_GENERAL_NEWLINE; }
-unsigned int get_onig_syn_op2_esc_capital_n_o_super_dot() { return ONIG_SYN_OP2_ESC_CAPITAL_N_O_SUPER_DOT; }
-unsigned int get_onig_syn_op2_atmark_capture_history() { return ONIG_SYN_OP2_ATMARK_CAPTURE_HISTORY; }
-unsigned int get_onig_syn_op2_qmark_brace_callout_contents() { return ONIG_SYN_OP2_QMARK_BRACE_CALLOUT_CONTENTS; }
+DEFINE_SYNTAX_GETTER(asis, ONIG_SYNTAX_ASIS)
+DEFINE_SYNTAX_GETTER(posix_basic, ONIG_SYNTAX_POSIX_BASIC)
+DEFINE_SYNTAX_GETTER(posix_extended, ONIG_SYNTAX_POSIX_EXTENDED)
+DEFINE_SYNTAX_GETTER(emacs, ONIG_SYNTAX_EMACS)
+DEFINE_SYNTAX_GETTER(grep, ONIG_SYNTAX_GREP)
+DEFINE_SYNTAX_GETTER(gnu_regex, ONIG_SYNTAX_GNU_REGEX)
+DEFINE_SYNTAX_GETTER(java, ONIG_SYNTAX_JAVA)
+DEFINE_SYNTAX_GETTER(perl, ONIG_SYNTAX_PERL)
+DEFINE_SYNTAX_GETTER(perl_ng, ONIG_SYNTAX_PERL_NG)
+DEFINE_SYNTAX_GETTER(ruby, ONIG_SYNTAX_RUBY)
+DEFINE_SYNTAX_GETTER(oniguruma, ONIG_SYNTAX_ONIGURUMA)
+DEFINE_SYNTAX_GETTER(default_syntax, ONIG_SYNTAX_DEFAULT)
 
-unsigned int get_onig_syn_allow_unmatched_close_subexp() { return ONIG_SYN_ALLOW_UNMATCHED_CLOSE_SUBEXP; }
-unsigned int get_onig_syn_allow_interval_low_abbrev() { return ONIG_SYN_ALLOW_INTERVAL_LOW_ABBREV; }
-unsigned int get_onig_syn_allow_empty_range_in_cc() { return ONIG_SYN_ALLOW_EMPTY_RANGE_IN_CC; }
-unsigned int get_onig_syn_warn_cc_op_not_escaped() { return ONIG_SYN_WARN_CC_OP_NOT_ESCAPED; }
+DEFINE_UINT_GETTER(option_extend, ONIG_OPTION_EXTEND)
+DEFINE_UINT_GETTER(option_multiline, ONIG_OPTION_MULTILINE)
+DEFINE_UINT_GETTER(option_singleline, ONIG_OPTION_SINGLELINE)
+DEFINE_UINT_GETTER(option_find_longest, ONIG_OPTION_FIND_LONGEST)
+DEFINE_UINT_GETTER(option_find_not_empty, ONIG_OPTION_FIND_NOT_EMPTY)
+DEFINE_UINT_GETTER(option_negate_singleline, ONIG_OPTION_NEGATE_SINGLELINE)
+DEFINE_UINT_GETTER(option_dont_capture_group, ONIG_OPTION_DONT_CAPTURE_GROUP)
+DEFINE_UINT_GETTER(option_capture_group, ONIG_OPTION_CAPTURE_GROUP)
+DEFINE_UINT_GETTER(option_notbol, ONIG_OPTION_NOTBOL)
+DEFINE_UINT_GETTER(option_noteol, ONIG_OPTION_NOTEOL)
+DEFINE_UINT_GETTER(option_not_begin_string, ONIG_OPTION_NOT_BEGIN_STRING)
+DEFINE_UINT_GETTER(option_not_end_string, ONIG_OPTION_NOT_END_STRING)
+DEFINE_UINT_GETTER(option_not_begin_position, ONIG_OPTION_NOT_BEGIN_POSITION)
+
+DEFINE_UINT_GETTER(syn_op_variable_meta_characters, ONIG_SYN_OP_VARIABLE_META_CHARACTERS)
+DEFINE_UINT_GETTER(syn_op_dot_anychar, ONIG_SYN_OP_DOT_ANYCHAR)
+DEFINE_UINT_GETTER(syn_op_asterisk_zero_inf, ONIG_SYN_OP_ASTERISK_ZERO_INF)
+DEFINE_UINT_GETTER(syn_op_esc_asterisk_zero_inf, ONIG_SYN_OP_ESC_ASTERISK_ZERO_INF)
+DEFINE_UINT_GETTER(syn_op_plus_one_inf, ONIG_SYN_OP_PLUS_ONE_INF)
+DEFINE_UINT_GETTER(syn_op_esc_plus_one_inf, ONIG_SYN_OP_ESC_PLUS_ONE_INF)
+DEFINE_UINT_GETTER(syn_op_qmark_zero_one, ONIG_SYN_OP_QMARK_ZERO_ONE)
+DEFINE_UINT_GETTER(syn_op_esc_qmark_zero_one, ONIG_SYN_OP_ESC_QMARK_ZERO_ONE)
+DEFINE_UINT_GETTER(syn_op_bracket_cc, ONIG_SYN_OP_BRACKET_CC)
+DEFINE_UINT_GETTER(syn_op_esc_brace_interval, ONIG_SYN_OP_ESC_BRACE_INTERVAL)
+DEFINE_UINT_GETTER(syn_op_vbar_alt, ONIG_SYN_OP_VBAR_ALT)
+DEFINE_UINT_GETTER(syn_op_esc_vbar_alt, ONIG_SYN_OP_ESC_VBAR_ALT)
+DEFINE_UINT_GETTER(syn_op_lparen_subexp, ONIG_SYN_OP_LPAREN_SUBEXP)
+DEFINE_UINT_GETTER(syn_op_esc_lparen_subexp, ONIG_SYN_OP_ESC_LPAREN_SUBEXP)
+DEFINE_UINT_GETTER(syn_op_esc_o_brace_octal, ONIG_SYN_OP_ESC_O_BRACE_OCTAL)
+
+DEFINE_UINT_GETTER(syn_op2_qmark_lt_named_group, ONIG_SYN_OP2_QMARK_LT_NAMED_GROUP)
+DEFINE_UINT_GETTER(syn_op2_esc_p_brace_circumflex_not, ONIG_SYN_OP2_ESC_P_BRACE_CIRCUMFLEX_NOT)
+DEFINE_UINT_GETTER(syn_op2_esc_x_y_text_segment, ONIG_SYN_OP2_ESC_X_Y_TEXT_SEGMENT)
+DEFINE_UINT_GETTER(syn_op2_esc_v_vtab, ONIG_SYN_OP2_ESC_V_VTAB)
+DEFINE_UINT_GETTER(syn_op2_esc_u_hex4, ONIG_SYN_OP2_ESC_U_HEX4)
+DEFINE_UINT_GETTER(syn_op2_esc_h_xdigit, ONIG_SYN_OP2_ESC_H_XDIGIT)
+DEFINE_UINT_GETTER(syn_op2_esc_k_named_backref, ONIG_SYN_OP2_ESC_K_NAMED_BACKREF)
+DEFINE_UINT_GETTER(syn_op2_esc_g_subexp_call, ONIG_SYN_OP2_ESC_G_SUBEXP_CALL)
+DEFINE_UINT_GETTER(syn_op2_qmark_lparen_if_else, ONIG_SYN_OP2_QMARK_LPAREN_IF_ELSE)
+DEFINE_UINT_GETTER(syn_op2_esc_capital_r_general_newline, ONIG_SYN_OP2_ESC_CAPITAL_R_GENERAL_NEWLINE)
+DEFINE_UINT_GETTER(syn_op2_esc_capital_n_o_super_dot, ONIG_SYN_OP2_ESC_CAPITAL_N_O_SUPER_DOT)
+DEFINE_UINT_GETTER(syn_op2_atmark_capture_history, ONIG_SYN_OP2_ATMARK_CAPTURE_HISTORY)
+DEFINE_UINT_GETTER(syn_op2_qmark_brace_callout_contents, ONIG_SYN_OP2_QMARK_BRACE_CALLOUT_CONTENTS)
+
+DEFINE_UINT_GETTER(syn_allow_unmatched_close_subexp, ONIG_SYN_ALLOW_UNMATCHED_CLOSE_SUBEXP)
+DEFINE_UINT_GETTER(syn_allow_interval_low_abbrev, ONIG_SYN_ALLOW_INTERVAL_LOW_ABBREV)
+DEFINE_UINT_GETTER(syn_allow_empty_range_in_cc, ONIG_SYN_ALLOW_EMPTY_RANGE_IN_CC)
+DEFINE_UINT_GETTER(syn_warn_cc_op_not_escaped, ONIG_SYN_WARN_CC_OP_NOT_ESCAPED)
+
+#undef DEFINE_ENCODING_GETTER
+#undef DEFINE_SYNTAX_GETTER
+#undef DEFINE_UINT_GETTER
